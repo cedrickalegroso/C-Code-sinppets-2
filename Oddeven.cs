8e29911b82f51a4c701d80aa123401c72cbb0c33
Cedrick Alegroso <42290274+cedrickalegroso@users.noreply.github.com>
2020-09-11 14:46:33 +0800

    ++september11 activity

---
+++ Oddeven.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alegroso_Activity3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, odd = 0, even = 0, totaleven =0, totalodd = 0, backupeven, backupodd;
+
+            List<float> evenarr = new List<float>();
+            List<float> oddarr = new List<float>();
+
+
+            Console.Write("Enter the min:" );
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the max:");
+            n2 = int.Parse(Console.ReadLine());
+
+            while(n1 <= n2)
+            {
+               
+                if (n1 % 2 == 0 )
+                {
+                    
+                    evenarr.Add(n1);  // add the number to array
+                    backupeven = n1; // back up so we can go back to the current n1?
+                    totaleven = n1 + totaleven; // add it on the current value 
+                    even++;  // add even 
+                    n1 = backupeven; // go back to the saved state :) 
+                } else
+                {
+                   
+                    oddarr.Add(n1); // add the number to array
+                    backupodd = n1; // back up so we can go back to the current n1?
+                    totalodd = n1 + totalodd; // add it on the current value 
+                    odd++;       // add odd      
+                    backupodd = n1;  // go back to the saved state :) 
+                }
+                // Console.WriteLine(n1);
+                ++n1;
+            }
+
+       
+            /// print the sum of all even
+
+            Console.Write("The sum of all EVEN numbers ");
+
+           
+            for (int eveni = 0; eveni < evenarr.Count; eveni++)
+            {
+                Console.Write(" " + evenarr[eveni]);
+            }
+
+            Console.Write(" is {0} ", totaleven);
+
+            
+
+            Console.WriteLine("");
+
+            /// print the sum of all odds
+
+            Console.Write("The sum of all ODD numbers ");
+
+            for (int oddi = 0; oddi < oddarr.Count; oddi++)
+            {
+                Console.Write(" " + oddarr[oddi]);
+            }
+
+            Console.Write(" is {0} ", totalodd);
+
+
+            Console.WriteLine("");
+
+
+            if (totalodd > totaleven)
+            {
+                Console.WriteLine("Higher  Number: ODD");
+                Console.WriteLine("Lower   Number: Even");
+            } else
+            {
+                Console.WriteLine("Higher  Number: Even");
+                Console.WriteLine("Lower   Number: ODD");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lastname_Activity3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2;
+            List<float> arraynum = new List<float>();
+
+            Console.Write("Enter min:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter max:");
+            num2 = int.Parse(Console.ReadLine());
+
+            while (num1 <= num2)
+            {
+                //Console.Write(arraynum);
+                arraynum.Add(num1);
+                num1++;
+            }
+
+            Console.Write(" Array = [");
+
+            for (int arrayi = 0; arrayi < arraynum.Count; arrayi++)
+            {
+                Console.Write(" " + arraynum[arrayi] + ",");
+            }
+
+            Console.Write(" ] ");
+
+        }
+    }
+}
